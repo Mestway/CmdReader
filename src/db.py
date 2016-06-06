50d7424da7cdebf5c478c97b0453437fadc65f36
Calvin Loncaric <loncaric@cs.washington.edu>
2016-06-06 15:08:22 -0700

    Added basic database functions

---
+++ src/db.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+class DBConnection(object):
+    def __init__(self):
+        self.conn = sqlite3.connect("data.db")
+        c = self.conn.cursor()
+        c.execute("CREATE TABLE IF NOT EXISTS NoPairs (url TEXT PRIMARY KEY, user_id INT) WITHOUT ROWID")
+        c.execute("CREATE TABLE IF NOT EXISTS Pairs   (url TEXT PRIMARY KEY, user_id INT, nl TEXT, cmd TEXT) WITHOUT ROWID")
+        self.conn.commit()
+
+    def __enter__(self, *args, **kwargs):
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        self.conn.commit()
+        self.conn.close()
+
+    def mark_has_no_pairs(self, url, user_id):
+        c = self.conn.cursor()
+        c.execute("INSERT INTO NoPairs (url, user_id) VALUES (?, ?)", (url, user_id))
+        self.conn.commit()
+
+    def add_pair(self, url, user_id, nl_text, cmd_text):
+        c = self.conn.cursor()
+        c.execute("INSERT INTO Pairs (url, user_id, nl, cmd) VALUES (?, ?, ?, ?)", (url, user_id, nl_text, cmd_text))
+        self.conn.commit()
+
+    def pairs(self):
+        c = self.conn.cursor()
+        for nl, cmd in c.execute("SELECT nl, cmd FROM Pairs"):
+            yield (nl, cmd)
--- src/server.py
+++ src/server.py
@@ -1,8 +1,13 @@
 import cherrypy
+from db import DBConnection
 
 class App(object):
     @cherrypy.expose
     def index(self):
-        return "Hello World!"
+        with DBConnection() as db:
+            out = "Hello!<br>"
+            for nl, cmd in db.pairs():
+                out += "Pair: {}, {}<br>".format(nl, cmd)
+            return out
 
 cherrypy.quickstart(App())
