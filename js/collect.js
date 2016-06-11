a30b6095fdf9f64fcebee454ae66944a5468c39b
Chenglong <mestway@gmail.com>
2016-06-11 14:16:54 -0700

    ok

--- js/collect.js
+++ js/collect.js
@@ -4,11 +4,12 @@ var myLayout;
 $(document).ready(function(){
 
 	myLayout = $('body').layout({
-		west__minSize:	200,
+		west__minSize: 200,
 		west__size:	"40%",
 		west__onresize_end: function () {
+			$("#nl2cmd-web-content-panel")
+      .html('<object id="web-content-data" height="100%" data="http://www.uwplse.org"/>');
     	$("#web-content-data").width($("body").width()-$("#nl2cmd-web-working-panel").width())
-    	console.log($("body").height);
     },
 
 	});
