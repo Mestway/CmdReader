27c5651e7ad726d5bbc93fbd0f4eeab40280b3f4
Chenglong <mestway@gmail.com>
2016-06-11 12:20:13 -0700

    front page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
   $('#nl2cmd-keyword-search-button').click(function () {
-    console.log("wowow!");	
+    var search_query = $('#nl2cmd-keyword-search-box').val();
+    console.log(search_query);
+	});
+
+	$('#nl2cmd-auto-redirect').click(function () {
+    var url = $.get( "/service", function( data ) {
+		  console.log( "Data Loaded: " + data );
+		});
 	});
 });
-
