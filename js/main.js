716a13475bdff7bbe4bc3723c5adc55f8a176b86
Calvin Loncaric <loncaric@cs.washington.edu>
2016-06-11 14:52:35 -0700

    Call /pick_url when we need a new URL for the user

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
 $(document).ready(function () {
+
+  function start(query) {
+    $.getJSON("/pick_url", {search_phrase: query}, function(url) {
+      console.log(url);
+      if (url === null) {
+        alert("no URLs available!");
+      } else {
+        alert("TODO: you should be redirected to page 2");
+      }
+    });
+  }
+
+
   $('#nl2cmd-keyword-search-button').click(function () {
     var search_query = $('#nl2cmd-keyword-search-box').val();
-    console.log(search_query);
+    start(search_query);
 	});
 
 	$('#nl2cmd-auto-redirect').click(function () {
-    var url = $.get( "/service", function( data ) {
-		  console.log(data);
-		});
+    start("");
 	});
 });
