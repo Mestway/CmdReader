2ddd83efb0bb9ce852f71facc0540e003b2ec200
Calvin Loncaric <loncaric@cs.washington.edu>
2016-06-11 11:41:23 -0700

    Set up CherryPy config with static paths to bootstrap resources

--- src/server.py
+++ src/server.py
@@ -1,5 +1,21 @@
+import os
 import cherrypy
 from db import DBConnection
+
+# Root location where we can find resource files.
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+config = {
+    "/js": {
+        "tools.staticdir.on": True,
+        "tools.staticdir.dir": os.path.join(ROOT, "js") },
+    "/css": {
+        "tools.staticdir.on": True,
+        "tools.staticdir.dir": os.path.join(ROOT, "css") },
+    "/fonts": {
+        "tools.staticdir.on": True,
+        "tools.staticdir.dir": os.path.join(ROOT, "fonts") },
+}
 
 class App(object):
     @cherrypy.expose
@@ -10,4 +26,5 @@ class App(object):
                 out += "Pair: {}, {}<br>".format(nl, cmd)
             return out
 
-cherrypy.quickstart(App())
+if __name__ == "__main__":
+    cherrypy.quickstart(App(), config=config)
