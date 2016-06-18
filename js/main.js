c8f7803b5215339efbd785eb8d9c848f627d5f2b
Victoria Lin <purenanya@gmail.com>
2016-06-18 12:31:40 -0700

    mf

--- js/main.js
+++ js/main.js
@@ -7,26 +7,29 @@ $(document).ready(function () {
         alert("no URLs available!");
         window.location.replace("./collect_page.html?url=http://www.uwplse.org");
       } else {
+        console.log(url)
         // similar behavior as an HTTP redirect
         window.location.replace("./collect_page.html?url=" + url);
       }
     });
   }
 
-  // trigger search on "Enter"
+  // trigger search by pressing "Enter" key
   $("#nl2cmd-keyword-search-box").keyup(function(event){
     if(event.keyCode == 13){
-        $("#nl2cmd-keyword-search-button").click();
+      $("#nl2cmd-keyword-search-button").click();
     }
   });
 
-  $('#nl2cmd-keyword-search-button').click(function () {
+  $('#nl2cmd-keyword-search-button').click(function() {
     var search_query = $('#nl2cmd-keyword-search-box').val();
-    start(search_query);
-	});
+    if (search_query) {         // ignore empty search query
+        start(search_query);
+    }
+  });
 
-	$('#nl2cmd-auto-redirect').click(function () {
-    start("");
-	});
+  $('#nl2cmd-auto-redirect').click(function(){
+    start("RANDOM_SELECTION");
+  });
 
 });
