14a55cec6f1f72ebf52d4c8752c8c1cf01029aec
Victoria Lin <purenanya@gmail.com>
2016-07-07 11:54:04 -0700

    mf

--- src/db.py
+++ src/db.py
@@ -173,8 +173,7 @@ class DBConnection(object):
     def commands(self):
         c = self.conn.cursor()
         for cmd in c.execute("SELECT DISTINCT cmd FROM Pairs"):
-            if "find" in cmd:
-                yield cmd
+            yield cmd
         c.close()
 
     # --- Query management ---
