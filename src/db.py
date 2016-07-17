34bbaa15498e0a2e40404818bb4c06384645d1dd
Victoria Lin <xilin@cs.washington.edu>
2016-07-16 18:51:06 -0700

    change url average score threshold

--- src/db.py
+++ src/db.py
@@ -39,6 +39,8 @@ EDITDIST_THRESH = 8
 # minimum number of auto-detected commands a page has to contain
 NUM_CMDS_THRESH = 5
 
+AVG_SCORE_THRESH = 0.5
+
 # Depending on how often we expect to be doing server updates, we might want to
 # make this information persistent.
 url_leases = []
@@ -477,7 +479,7 @@ class DBConnection(object):
                                               # "ORDER BY avg_score DESC " +
                                               "WHERE num_visits = ? " +
                                               # "AND num_cmds >= ? ", (n, NUM_CMDS_THRESH)):
-                                              "AND num_cmds >= ? AND avg_score >= ?", (n, NUM_CMDS_THRESH, 0.3)):
+                                              "AND num_cmds >= ? AND avg_score >= ?", (n, NUM_CMDS_THRESH, AVG_SCORE_THRESH)):
             yield (url, count)
 
     def find_unannotated_urls(self):
@@ -486,7 +488,7 @@ class DBConnection(object):
                                               # "ORDER BY avg_score DESC "
                                               "WHERE num_visits = 0 " +
                                               # "AND num_cmds >= ? ", (NUM_CMDS_THRESH,)):
-                                              "AND num_cmds >= ? AND avg_score >= ?", (NUM_CMDS_THRESH, 0.3)):
+                                              "AND num_cmds >= ? AND avg_score >= ?", (NUM_CMDS_THRESH, AVG_SCORE_THRESH)):
             yield (url, count)
 
     def num_urls_by_num_visit(self, n):
