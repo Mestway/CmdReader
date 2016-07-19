1f5faaf03192858018edf1edefb292318d3520c1
Victoria Lin <xilin@cs.washington.edu>
2016-07-18 23:26:34 -0700

    edit distance bug fix

--- src/db.py
+++ src/db.py
@@ -56,8 +56,8 @@ def distance(f1, f2):
         x &= x - 1
     return ans
 
-def minEditDist(target, source):
-    ''' Computes the min edit distance from target to source. Figure 3.25 '''
+def  minEditDist(target, source):
+    ''' Computes the min edit distance from target to source. '''
 
     n = len(target)
     m = len(source)
@@ -65,17 +65,27 @@ def minEditDist(target, source):
     distance = [[0 for i in range(m+1)] for j in range(n+1)]
 
     for i in range(1,n+1):
-        distance[i][0] = distance[i-1][0] + 1
+        distance[i][0] = distance[i-1][0] + insertCost(target[i-1])
 
     for j in range(1,m+1):
-        distance[0][j] = distance[0][j-1] + 1
+        distance[0][j] = distance[0][j-1] + deleteCost(source[j-1])
 
     for i in range(1,n+1):
         for j in range(1,m+1):
            distance[i][j] = min(distance[i-1][j]+1,
                                 distance[i][j-1]+1,
-                                distance[i-1][j-1]+1)
+                                distance[i-1][j-1]+substCost(source[j-1],target[i-1]))
     return distance[n][m]
+
+def insertCost(x):
+    return 1
+
+def deleteCost(x):
+    return 1
+
+def substCost(x,y):
+    if x == y: return 0
+    else: return 2
 
 def ensure_unicode(content):
     if isinstance(content, str):
